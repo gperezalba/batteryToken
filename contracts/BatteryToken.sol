f4f31798cd528e9d7082b32eb3d0352d35e5be7b
Guillermo Pérez Alba <gperezalba94@gmail.com>
2019-02-18 11:00:56 +0100

    Add coments in contract

--- contracts/BatteryToken.sol
+++ contracts/BatteryToken.sol
@@ -3,52 +3,49 @@ pragma solidity >=0.4.21 <0.6.0;
 import "./SafeMath.sol";
 import "./ERC20.sol";
 
+/// @author Guillermo Pérez Alba
+/// @title A token designed to exchange motocycle's batteries
 contract BatteryToken is ERC20 {
     using SafeMath for uint256;
 
     //Añadir el cambio de ownership y revisar visibilidad de todo
 
-    //Variables
-    address public owner; //Owner of the contract
-    uint256 public timeRef; //Time reference for the hole contract
-    uint256 public globalId; //Unique identifier for batteries
-    uint256 constant CHARGEPRICE = 7; //Final value depends on the lifeTime of the battery
-    uint256 constant PROPOSALTIME = 10 * 1 minutes; //Max time since proposal until execution
+    address public owner;
+    uint256 public timeRef;
+    uint256 public globalId;
+    uint256 constant CHARGEPRICE = 7;
+    uint256 constant PROPOSALTIME = 10 * 1 minutes;
 
-    //struct
     struct Battery {
-        uint256 initialValue; //Value of the battery the 0 day
-        bool publicDomain; //True si la bateria es publica False privada//cambiar por enum?
-        address privateCharger; //If private the battery can be assigned temporary to a charger
-        uint256 startTime; //0 day of the battery
+        uint256 initialValue;
+        bool publicDomain; //True for public. False for private
+        address privateCharger;
+        uint256 startTime;
     }
 
     struct Exchange {
-        uint256 itemProposer; //Id of the battery that owns the proposer
-        uint256 itemExecuter; //Id of the battery that owns the executer (if 0 is a Sell)
+        uint256 itemProposer;
+        uint256 itemExecuter;
         address proposer;
         address executer;
-        uint256 valueProposer; //Vale of the battery that owns the proposer
-        uint256 valueExecuter; //Value of the battery that owns the executer (if 0 is a Sell)
-        uint256 proposedTime; //Time of proposal
-        bool proposed; //True when the exchange is proposed
-        bool executed; //True when the exchange is executed
+        uint256 valueProposer;
+        uint256 valueExecuter;
+        uint256 proposedTime;
+        bool proposed;
+        bool executed;
     }
 
-    //mapping
     mapping(uint256 => address) _batteryOwner;
     mapping(uint256 => Battery) _battery;
     mapping(address => uint256[]) _batteriesByOwner;
-    mapping(uint256 => uint256) _indexOfId; //starting since 1, when using use index-1
+    mapping(uint256 => uint256) _indexOfId; //starting since 1, use index-1
     mapping(bytes32 => Exchange) _exchanges;
 
-    //event
     event MintBat(address own, uint256 id, bool domain, uint time);
     event BurnBat(address own, uint256 id);
     event Proposal(bytes32 proposalId, address emiter, address executer, uint256 itemEmiter, uint256 itemExecuter);
     event Execution(address emiter, address executer, uint256 itemEmiter, uint256 itemExecuter);
 
-    //Functions
     constructor() public {
         owner = msg.sender;
         timeRef = block.timestamp;
@@ -59,19 +56,30 @@ contract BatteryToken is ERC20 {
         _indexOfId[globalId] = _batteriesByOwner[address(0)].push(globalId);
     }
 
+    /// @dev Return the IDs of the batteries owned by an owner
+    /// @param _owner the address of the owner
+    /// @return an array with the IDs of the batteries
     function batteriesOfOwner(address _owner) public view returns (uint256[]) {
         return _batteriesByOwner[_owner];
     }
 
-    //Returns the owner of a battery. If address(0) battery doesn't exist
+    /// @dev Return the owner of a certain battery ID
+    /// @param id the ID of the battery
+    /// @return the address of the owner. When address(0) battery doesn't exist
     function ownerOf(uint256 id) public view returns (address) {
         return _batteryOwner[id];
     }
 
+    /// @dev Return the details of a certain battery ID
+    /// @param id the ID of the battery
     function getBattery(uint256 id) public view returns (bool, uint256, uint256, address) {
         return (_battery[id].publicDomain, _battery[id].startTime, _battery[id].initialValue, _battery[id].privateCharger);
     }
 
+    /// @dev Mints a new battery with the next Global ID
+    /// @param _publicDomain True when battery is public, False when private
+    /// @param _value Initial value of the battery
+    /// @return the ID of the new battery
     function mintBat(bool _publicDomain, uint256 _value) public returns (uint256) {
         //todos pueden añadir baterias? hacer un require para solo algunas address autorizadas?
         globalId = globalId.add(1);
@@ -84,6 +92,8 @@ contract BatteryToken is ERC20 {
         return globalId;
     }
 
+    /// @dev The battery stops existing in the contract
+    /// @param id the ID of the battery
     function burnBat(uint256 id) public returns (bool) {
         //Si el mint lo hacen las fabricas los puntos de carga se las compran y cuando estas tengan un valor
         //muuy bajo se las vuelven a vender y las fabricas hacen el burn. Si alguien hace burn antes pierde ese dinero
@@ -95,6 +105,9 @@ contract BatteryToken is ERC20 {
         return true;
     }
 
+    /// @dev Remove a battery of the array of batteries for an owner
+    /// @param _id the ID of the battery
+    /// @param _owner the address of the current owner
     function removeFromArray (uint256 _id, address _owner) private returns (bool) {
         uint256 index = _indexOfId[_id];
         if (index == 0) return;
@@ -106,6 +119,13 @@ contract BatteryToken is ERC20 {
         return true;
     }
 
+    /// @dev Propose an exchange of batteries (or token) to another address
+    /// @param _itemProposer ID of the battery the proposer exchanges
+    /// @param _itemExecuter ID of the battery the executer exchanges. Is 0 when Sell or Private Exchange
+    /// @param proposerChargeLevel Current level of the battery owned by the proposer (between 0-100)
+    /// @param executerChargeLevel Current level of the battery owned by the executer (between 0-100). Is 0 when Sell or Private Exchange
+    /// @param _executer the address of the executer
+    /// @return a bytes32 with the ID of the exchange
     function proposeExchange(
         uint256 _itemProposer,
         uint256 _itemExecuter,
@@ -140,6 +160,8 @@ contract BatteryToken is ERC20 {
         return _exchangeId;
     }
 
+    /// @dev Execute an exchange previously proposed
+    /// @param _exchangeId the ID of the exchange to execute
     function executeExchange(bytes32 _exchangeId) public returns (bool) {
         require(_exchanges[_exchangeId].proposed, "This exchange id does not exist");
         //meter un !executed
@@ -172,6 +194,10 @@ contract BatteryToken is ERC20 {
         emit Execution(_exchanges[_exchangeId].proposer, msg.sender, _exchanges[_exchangeId].itemProposer, _exchanges[_exchangeId].itemExecuter);
     }
 
+    /// @dev Return the value of a certain battery ID with a certain level of charge.
+    /// @param _itemId the ID of the battery
+    /// @param _itemChargeLevel the current level of charge of the battery (between 0-100)
+    /// @return the current value of the battery
     function getBatValue(uint256 _itemId, uint256 _itemChargeLevel) public view returns(uint256){
         if (_itemId == 0) {
             return 0;
@@ -191,6 +217,8 @@ contract BatteryToken is ERC20 {
 
     }
 
+    /// @dev Mints a certain amount of ERC20 token to the msg.sender
+    /// @param amount the amount of tokens to mint 
     function mintERC20(uint256 amount) public returns(bool){
         super._mint(msg.sender, amount);
         return true;
