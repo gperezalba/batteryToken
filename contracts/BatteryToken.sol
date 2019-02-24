4a905be1aa6806749b3d50ab688130a39c50ef1c
Guillermo Pérez Alba <gperezalba94@gmail.com>
2019-02-24 12:21:23 +0100

    solc and variable names

--- contracts/BatteryToken.sol
+++ contracts/BatteryToken.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity 0.4.25;
 
 import "./SafeMath.sol";
 import "./ERC20.sol";
@@ -35,11 +35,11 @@ contract BatteryToken is ERC20 {
         bool executed;
     }
 
-    mapping(uint256 => address) _batteryOwner;
-    mapping(uint256 => Battery) _battery;
-    mapping(address => uint256[]) _batteriesByOwner;
-    mapping(uint256 => uint256) _indexOfId; //starting since 1, use index-1
-    mapping(bytes32 => Exchange) _exchanges;
+    mapping(uint256 => address) batteryOwner;
+    mapping(uint256 => Battery) battery;
+    mapping(address => uint256[]) batteriesByOwner;
+    mapping(uint256 => uint256) indexOfId; //starting since 1, use index-1
+    mapping(bytes32 => Exchange) exchanges;
 
     event MintBat(address own, uint256 id, bool domain, uint time);
     event BurnBat(address own, uint256 id);
@@ -49,169 +49,167 @@ contract BatteryToken is ERC20 {
     constructor() public {
         owner = msg.sender;
         timeRef = block.timestamp;
-        _batteryOwner[globalId] = address(0);
-        _battery[globalId].publicDomain = false;
-        _battery[globalId].startTime = block.timestamp;
-        _battery[globalId].initialValue = 0;
-        _indexOfId[globalId] = _batteriesByOwner[address(0)].push(globalId);
+        batteryOwner[globalId] = address(0);
+        battery[globalId].publicDomain = false;
+        battery[globalId].startTime = block.timestamp;
+        battery[globalId].initialValue = 0;
+        indexOfId[globalId] = batteriesByOwner[address(0)].push(globalId);
     }
 
     /// @dev Return the IDs of the batteries owned by an owner
-    /// @param _owner the address of the owner
+    /// @param who the address of the owner
     /// @return an array with the IDs of the batteries
-    function batteriesOfOwner(address _owner) public view returns (uint256[]) {
-        return _batteriesByOwner[_owner];
+    function batteriesOfOwner(address who) public view returns (uint256[]) {
+        return batteriesByOwner[who];
     }
 
     /// @dev Return the owner of a certain battery ID
     /// @param id the ID of the battery
     /// @return the address of the owner. When address(0) battery doesn't exist
     function ownerOf(uint256 id) public view returns (address) {
-        return _batteryOwner[id];
+        return batteryOwner[id];
     }
 
     /// @dev Return the details of a certain battery ID
     /// @param id the ID of the battery
     function getBattery(uint256 id) public view returns (bool, uint256, uint256, address) {
-        return (_battery[id].publicDomain, _battery[id].startTime, _battery[id].initialValue, _battery[id].privateCharger);
+        return (battery[id].publicDomain, battery[id].startTime, battery[id].initialValue, battery[id].privateCharger);
     }
 
     /// @dev Mints a new battery with the next Global ID
-    /// @param _publicDomain True when battery is public, False when private
-    /// @param _value Initial value of the battery
+    /// @param domain True when battery is public, False when private
+    /// @param value Initial value of the battery
     /// @return the ID of the new battery
-    function mintBat(bool _publicDomain, uint256 _value) public returns (uint256) {
+    function mintBat(bool domain, uint256 value) public returns (uint256) {
         //todos pueden anhadir baterias? hacer un require para solo algunas address autorizadas?
         globalId = globalId.add(1);
-        _batteryOwner[globalId] = msg.sender;
-        _battery[globalId].publicDomain = _publicDomain;
-        _battery[globalId].startTime = block.timestamp;
-        _battery[globalId].initialValue = _value;
-        _indexOfId[globalId] = _batteriesByOwner[msg.sender].push(globalId);
-        emit MintBat(_batteryOwner[globalId], globalId, _battery[globalId].publicDomain, _battery[globalId].startTime);
+        batteryOwner[globalId] = msg.sender;
+        battery[globalId].publicDomain = domain;
+        battery[globalId].startTime = block.timestamp;
+        battery[globalId].initialValue = value;
+        indexOfId[globalId] = batteriesByOwner[msg.sender].push(globalId);
+        emit MintBat(batteryOwner[globalId], globalId, battery[globalId].publicDomain, battery[globalId].startTime);
         return globalId;
     }
 
     /// @dev The battery stops existing in the contract
     /// @param id the ID of the battery
-    function burnBat(uint256 id) public returns (bool) {
+    function burnBat(uint256 id) public {
         //Si el mint lo hacen las fabricas los puntos de carga se las compran y cuando estas tengan un valor
         //muuy bajo se las vuelven a vender y las fabricas hacen el burn. Si alguien hace burn antes pierde ese dinero
-        require(msg.sender == _batteryOwner[id], "Only the owner can burn");
-        _batteryOwner[id] = address(0);
+        require(msg.sender == batteryOwner[id], "Only the owner can burn");
+        batteryOwner[id] = address(0);
         removeFromArray(id, msg.sender);
         //Devolver valor en ether a su cuenta real en Ethers????
         emit BurnBat(msg.sender, id);
-        return true;
     }
 
     /// @dev Remove a battery of the array of batteries for an owner
-    /// @param _id the ID of the battery
-    /// @param _owner the address of the current owner
-    function removeFromArray (uint256 _id, address _owner) private returns (bool) {
-        uint256 index = _indexOfId[_id];
+    /// @param id the ID of the battery
+    /// @param who the address of the current owner
+    function removeFromArray (uint256 id, address who) private {
+        uint256 index = indexOfId[id];
         if (index == 0) return;
 
-        if(_batteriesByOwner[_owner].length > 1) {
-            _batteriesByOwner[_owner][index.sub(1)] = _batteriesByOwner[_owner][_batteriesByOwner[_owner].length.sub(1)];
+        if(batteriesByOwner[who].length > 1) {
+            batteriesByOwner[who][index.sub(1)] = batteriesByOwner[who][batteriesByOwner[who].length.sub(1)];
         }
-        _batteriesByOwner[_owner].length --;
-        return true;
+        batteriesByOwner[who].length --;
     }
 
     /// @dev Propose an exchange of batteries (or token) to another address
-    /// @param _itemProposer ID of the battery the proposer exchanges
-    /// @param _itemExecuter ID of the battery the executer exchanges. Is 0 when Sell or Private Exchange
+    /// @param itemProposer ID of the battery the proposer exchanges
+    /// @param itemExecuter ID of the battery the executer exchanges. Is 0 when Sell or Private Exchange
     /// @param proposerChargeLevel Current level of the battery owned by the proposer (between 0-100)
     /// @param executerChargeLevel Current level of the battery owned by the executer (between 0-100). Is 0 when Sell or Private Exchange
-    /// @param _executer the address of the executer
+    /// @param executer the address of the executer
     /// @return a bytes32 with the ID of the exchange
     function proposeExchange(
-        uint256 _itemProposer,
-        uint256 _itemExecuter,
+        uint256 itemProposer,
+        uint256 itemExecuter,
         uint256 proposerChargeLevel,
         uint256 executerChargeLevel,
-        address _executer
+        address executer
     )
         public
         returns (bytes32)
     {
-        require((msg.sender == _batteryOwner[_itemProposer] || msg.sender == _battery[_itemProposer].privateCharger), "Wrong msg.sender");
-        bytes32 _exchangeId = bytes32(keccak256(abi.encodePacked(block.timestamp, _itemProposer, _itemExecuter, msg.sender, _executer)));
-        _exchanges[_exchangeId].itemProposer = _itemProposer;
-        _exchanges[_exchangeId].proposer = msg.sender;
-        _exchanges[_exchangeId].executer = _executer;
-        if (_battery[_itemProposer].publicDomain) {
-            _exchanges[_exchangeId].itemExecuter = _itemExecuter;
-            _exchanges[_exchangeId].valueProposer = getBatValue(_itemProposer, proposerChargeLevel);
-            _exchanges[_exchangeId].valueExecuter = getBatValue(_itemExecuter, executerChargeLevel);
+        require((msg.sender == batteryOwner[itemProposer] || msg.sender == battery[itemProposer].privateCharger), "Wrong msg.sender");
+        bytes32 exchangeId = bytes32(keccak256(abi.encodePacked(block.timestamp, itemProposer, itemExecuter, msg.sender, executer)));
+        exchanges[exchangeId].itemProposer = itemProposer;
+        exchanges[exchangeId].proposer = msg.sender;
+        exchanges[exchangeId].executer = executer;
+        if (battery[itemProposer].publicDomain) {
+            exchanges[exchangeId].itemExecuter = itemExecuter;
+            exchanges[exchangeId].valueProposer = getBatValue(itemProposer, proposerChargeLevel);
+            exchanges[exchangeId].valueExecuter = getBatValue(itemExecuter, executerChargeLevel);
         } else {
-            _exchanges[_exchangeId].valueProposer = CHARGEPRICE.mul(proposerChargeLevel).div(100).mul(getBatValue(_itemProposer, 100)).div(_battery[_itemProposer].initialValue);
+            exchanges[exchangeId].valueProposer = CHARGEPRICE.mul(proposerChargeLevel).div(100).mul(getBatValue(itemProposer, 100)).div(battery[itemProposer].initialValue);
         }
 
-        if (_exchanges[_exchangeId].valueProposer <= _exchanges[_exchangeId].valueExecuter){
-            super.approve(_executer, _exchanges[_exchangeId].valueExecuter.sub(_exchanges[_exchangeId].valueProposer));
+        if (exchanges[exchangeId].valueProposer <= exchanges[exchangeId].valueExecuter){
+            super.approve(executer, exchanges[exchangeId].valueExecuter.sub(exchanges[exchangeId].valueProposer));
             //redefinir aqui la funcion approve para que si expira el tiempo pierda lo aprobado
             //o para no jugarsela con esto hacer que el mas caro proponga y el mas barato execute
         }
-        _exchanges[_exchangeId].proposedTime = block.timestamp;
-        _exchanges[_exchangeId].proposed = true;
-        emit Proposal(_exchangeId, msg.sender, _executer, _itemProposer, _itemExecuter);
-        return _exchangeId;
+        exchanges[exchangeId].proposedTime = block.timestamp;
+        exchanges[exchangeId].proposed = true;
+        emit Proposal(exchangeId, msg.sender, executer, itemProposer, itemExecuter);
+        return exchangeId;
     }
 
     /// @dev Execute an exchange previously proposed
-    /// @param _exchangeId the ID of the exchange to execute
-    function executeExchange(bytes32 _exchangeId) public returns (bool) {
-        require(_exchanges[_exchangeId].proposed, "This exchange id does not exist");
+    /// @param exchangeId the ID of the exchange to execute
+    function executeExchange(bytes32 exchangeId) public {
+        require(exchanges[exchangeId].proposed, "This exchange id does not exist");
         //meter un !executed
-        require(msg.sender == _exchanges[_exchangeId].executer, "msg.sender must be the executer");
-        require(block.timestamp.sub(_exchanges[_exchangeId].proposedTime) < PROPOSALTIME, "Proposal time expired");
-        if (_exchanges[_exchangeId].valueProposer >= _exchanges[_exchangeId].valueExecuter){
-            super.transfer(_exchanges[_exchangeId].proposer, _exchanges[_exchangeId].valueProposer.sub(_exchanges[_exchangeId].valueExecuter));
+        require(msg.sender == exchanges[exchangeId].executer, "msg.sender must be the executer");
+        require(block.timestamp.sub(exchanges[exchangeId].proposedTime) < PROPOSALTIME, "Proposal time expired");
+        if (exchanges[exchangeId].valueProposer >= exchanges[exchangeId].valueExecuter){
+            super.transfer(exchanges[exchangeId].proposer, exchanges[exchangeId].valueProposer.sub(exchanges[exchangeId].valueExecuter));
         } else {
-            super.transferFrom(_exchanges[_exchangeId].proposer, msg.sender, _exchanges[_exchangeId].valueExecuter.sub(_exchanges[_exchangeId].valueProposer));
+            super.transferFrom(exchanges[exchangeId].proposer, msg.sender, exchanges[exchangeId].valueExecuter.sub(exchanges[exchangeId].valueProposer));
         }
-        if (_battery[_exchanges[_exchangeId].itemProposer].publicDomain) {
-            if (_exchanges[_exchangeId].itemProposer != 0) {
-                removeFromArray(_exchanges[_exchangeId].itemProposer, _exchanges[_exchangeId].proposer);
-                _batteryOwner[_exchanges[_exchangeId].itemProposer] = msg.sender;
-                _indexOfId[_exchanges[_exchangeId].itemProposer] = _batteriesByOwner[msg.sender].push(_exchanges[_exchangeId].itemProposer);
+        if (battery[exchanges[exchangeId].itemProposer].publicDomain) {
+            if (exchanges[exchangeId].itemProposer != 0) {
+                removeFromArray(exchanges[exchangeId].itemProposer, exchanges[exchangeId].proposer);
+                batteryOwner[exchanges[exchangeId].itemProposer] = msg.sender;
+                indexOfId[exchanges[exchangeId].itemProposer] = batteriesByOwner[msg.sender].push(exchanges[exchangeId].itemProposer);
             }
-            if (_exchanges[_exchangeId].itemExecuter != 0) {
-                removeFromArray(_exchanges[_exchangeId].itemExecuter, msg.sender);
-                _batteryOwner[_exchanges[_exchangeId].itemExecuter] = _exchanges[_exchangeId].proposer;
-                _indexOfId[_exchanges[_exchangeId].itemExecuter] = _batteriesByOwner[_exchanges[_exchangeId].proposer].push(_exchanges[_exchangeId].itemExecuter);
+            if (exchanges[exchangeId].itemExecuter != 0) {
+                removeFromArray(exchanges[exchangeId].itemExecuter, msg.sender);
+                batteryOwner[exchanges[exchangeId].itemExecuter] = exchanges[exchangeId].proposer;
+                indexOfId[exchanges[exchangeId].itemExecuter] = batteriesByOwner[exchanges[exchangeId].proposer].push(exchanges[exchangeId].itemExecuter);
             }
-        } else if (msg.sender == _batteryOwner[_exchanges[_exchangeId].itemProposer]) {
+        } else if (msg.sender == batteryOwner[exchanges[exchangeId].itemProposer]) {
             //retirada
-            _battery[_exchanges[_exchangeId].itemProposer].privateCharger = address(0);
+            battery[exchanges[exchangeId].itemProposer].privateCharger = address(0);
         } else {
             //entrega al pto carga
-            _battery[_exchanges[_exchangeId].itemProposer].privateCharger = msg.sender;
+            battery[exchanges[exchangeId].itemProposer].privateCharger = msg.sender;
         }
-        _exchanges[_exchangeId].executed = true;
-        emit Execution(_exchanges[_exchangeId].proposer, msg.sender, _exchanges[_exchangeId].itemProposer, _exchanges[_exchangeId].itemExecuter);
+        exchanges[exchangeId].executed = true;
+        emit Execution(exchanges[exchangeId].proposer, msg.sender, exchanges[exchangeId].itemProposer, exchanges[exchangeId].itemExecuter);
     }
 
     /// @dev Return the value of a certain battery ID with a certain level of charge.
-    /// @param _itemId the ID of the battery
-    /// @param _itemChargeLevel the current level of charge of the battery (between 0-100)
+    /// @param itemId the ID of the battery
+    /// @param itemChargeLevel the current level of charge of the battery (between 0-100)
     /// @return the current value of the battery
-    function getBatValue(uint256 _itemId, uint256 _itemChargeLevel) public view returns(uint256){
-        if (_itemId == 0) {
+    function getBatValue(uint256 itemId, uint256 itemChargeLevel) public view returns(uint256){
+        if (itemId == 0) {
             return 0;
         } else {
-            uint256 value = _battery[_itemId].initialValue
-            .sub(block.timestamp.sub(_battery[_itemId].startTime))
+            uint256 value = battery[itemId].initialValue
+            .sub(block.timestamp.sub(battery[itemId].startTime))
             .add(
               CHARGEPRICE
-                .mul(_itemChargeLevel)
+                .mul(itemChargeLevel)
                 .div(100)
-                .mul(_battery[_itemId].initialValue.sub(block.timestamp.sub(_battery[_itemId].startTime)))
-                .div(_battery[_itemId].initialValue)
+                .mul(battery[itemId].initialValue.sub(block.timestamp.sub(battery[itemId].startTime)))
+                .div(battery[itemId].initialValue)
             );
-            require((value >= 0 || value <= _battery[_itemId].initialValue.add(CHARGEPRICE)), "Bad value");
+            require((value >= 0 || value <= battery[itemId].initialValue.add(CHARGEPRICE)), "Bad value");
             return value;
         }
 
